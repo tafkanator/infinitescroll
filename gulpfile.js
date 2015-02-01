0ed822184bcd39322f1872b80f5ff22555706bf0
Taavi <taavi.sangelgmail.com>
2015-02-01 16:26:45 +0200

    changed folder structure improved gulp for using livereload better compiling form less

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,47 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var less = require('gulp-less');
-var path = require('path');
+var connect = require('gulp-connect');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('process-scripts', function() {
+var outputDir = './app/gen';
+
+gulp.task('html', function () {
+	gulp.src('./app/*.html')
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function() {
 	// Single entry point to browserify
-	gulp.src('src/main.js')
+	gulp.src('./app/js/main.js')
 		.pipe(browserify({
 			insertGlobals : true,
 			debug : true
 		}))
-		.pipe(gulp.dest('./dist'))
+		.pipe(gulp.dest(outputDir))
+		.pipe(connect.reload());
 });
 
-gulp.task('process-less', function () {
-	gulp.src('./src/*.less')
-		.pipe(less({
-			paths: [ path.join(__dirname, 'less', 'includes') ]
-		}))
-		.pipe(gulp.dest('./dist'));
+gulp.task('less', function () {
+	gulp.src('./app/less/main.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(outputDir))
+		.pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-	gulp.watch('src/*.js', ['process-scripts']);
-	gulp.watch('src/*.less', ['process-less']);
+	gulp.watch('./app/js/*.js', ['js']);
+	gulp.watch('./app/less/*.less', ['less']);
+	gulp.watch(['./app/*.html'], ['html']);
 });
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true
+	});
+});
+
+gulp.task('default', ['js', 'less', 'connect', 'watch']);
